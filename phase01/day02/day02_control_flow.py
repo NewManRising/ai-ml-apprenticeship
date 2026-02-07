d142cb951eb365c3f72921596c8847fa95aa23d7
DM <punkrotten@hotmail.com>
2026-02-06 23:33:45 -0600

    day02 update

---
+++ phase01/day02/day02_control_flow.py
@@ -0,0 +1,11 @@
+# Comparisons:
+# Equal:             ==
+# Not Equal:         !=
+# Greater Than:      >
+# Less Than:         <
+# Greater or Equal:  >=
+# Les or Equal:      <=
+# Object Identity    is
+
+
+user_age = int(input("What is your age? "))
