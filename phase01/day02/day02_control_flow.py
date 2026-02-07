f02d267721a6f1786b34921e28236c7280f51550
DM <punkrotten@hotmail.com>
2026-02-07 02:31:08 -0600

    Completion of day02 exercise

--- phase01/day02/day02_control_flow.py
+++ phase01/day02/day02_control_flow.py
@@ -1,3 +1,7 @@
+import random
+
+#---------------------------------------------------------#
+
 # Comparisons:
 # Equal:             ==
 # Not Equal:         !=
@@ -7,5 +11,65 @@
 # Les or Equal:      <=
 # Object Identity    is
 
+# and
+# or
+# not
 
-user_age = int(input("What is your age? "))
+# False values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence, Ex, '', (), [].
+    # Any empty mapping. Ex, {}.
+
+
+
+print("Welcome!\n")
+
+number = random.SystemRandom()
+
+
+while True:
+    ask_age = input("What is your age? ")
+    try:
+        age = int(ask_age)
+        if age < 18:
+            print("You are a minor")
+        else:
+            print("You are an adult")
+        break
+    except ValueError:
+        print("Please enter a number")
+
+
+
+adult = True
+senior = 65
+
+if adult and age >= senior:
+    print("You are a senior")
+
+
+
+pick_num = number.randint(1, 10)
+
+while True:
+    user_input = input("Guess a number between 1 and 10 (press q to quit): ")
+    if user_input.lower() == 'q':
+        print("Quitting - Goodbye!")
+        break
+
+    try:
+        guess = int(user_input)
+        if guess < 1 or guess > 10:
+            print("Please choose a number between 1 and 10")
+        elif guess == pick_num:
+            print("You guessed correctly!")
+        else:
+            print("You guessed incorrectly!")
+    except ValueError:
+        print("Type a valid number")
+
+
+
+
