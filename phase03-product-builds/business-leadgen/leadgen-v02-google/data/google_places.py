13fb8c9365d3c861d600dcf91c767ff77afb7bb3
DM <punkrotten@hotmail.com>
2026-05-21 23:53:33 -0500

    Phase 03: Added Google Places normalization and cleaned backend lead responses

--- phase03-product-builds/business-leadgen/leadgen-v02-google/data/google_places.py
+++ phase03-product-builds/business-leadgen/leadgen-v02-google/data/google_places.py
@@ -13,16 +13,44 @@ def search_google_places(location, keyword):
     headers = {
     "Content-Type": "application/json",
     "X-Goog-Api-Key": PLACES_KEY,
-    "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating,places.userRatingCount,places.websiteUri,places.nationalPhoneNumber,places.id"
+    "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating,places.userRatingCount,places.types,places.websiteUri,places.nationalPhoneNumber,places.businessStatus,places.id"
 }
 
 
     params = {
     "textQuery": f"{keyword} in {location}",
-    "pageSize": 5
+    "pageSize": 5,
+    "strictTypeFiltering": True
 }
 
     response = requests.post(url, headers=headers, json=params)
     data = response.json()
-    print(data)
-    return data
+
+
+    def cleaned_json(results):
+        cleaned = []
+
+        for place in results:
+            business_url = place.get("websiteUri", "")
+            clean_url = business_url.split("?")[0]
+
+            business = {
+                "name": place.get("displayName", {}).get("text", ""),
+                "address": place.get("formattedAddress", ""),
+                "phone": place.get("nationalPhoneNumber", ""),
+                "website": clean_url,
+                "rating": place.get("rating", ""),
+                "reviews": place.get("userRatingCount", ""),
+                "status": place.get("businessStatus", ""),
+
+            }
+
+            cleaned.append(business)
+
+        return cleaned
+    raw_results = data.get("places", [])
+
+    cleaned_results = cleaned_json(raw_results)
+
+    return cleaned_results
+
